ebafa2cc38ec77cf734aeeec433b0b10aaa4345c
Matias <matyyassgonzalez89@gmail.com>
2021-06-23 21:26:53 -0300

    primera subida de archivos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
+import SideBar from './components/SideBar/SideBar';
+import Main from './components/Main/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div id="wrapper">
+    <SideBar/>
+    <div id="content-wrapper" class="d-flex flex-column">
+      <Main/>
+      <Footer/>
     </div>
+
+  </div>
   );
 }
 
